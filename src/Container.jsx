81d6f63c3df37ac8ea2a4772281c48e4428eccf1
yorgos_k <georgis.kakavas@gmail.com>
2018-12-31 23:27:04 +0200

    Initial api experiment.

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './styles.scss'
 
+const Header = ({userData}) => {
+	return (
+		<div>I have data</div>	
+	);
+};
+
 export default class Container extends React.Component{
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			userData: null
+		};
+	}
+
+	fetchData(user) {
+		fetch(`https://api.github.com/users/${user}`, {
+      method: 'get'
+    })
+    .then(response => response.json())
+    .then(jsonData => this.setState({userData: jsonData}))
+    .catch(err => {
+			console.log(err)
+    })
+	};
+
 	render() {
+		const {userData} = this.state;
 		return (
-			<div>Hello</div>
+			<div>
+				<div>Do your worst</div>
+				<input type='text' placeholder='Your github userName goes here'/>
+				<button onClick={() => this.fetchData('falenos')}>Generate CV</button>
+				{userData && <Header userData={userData}/>}
+			</div>
 		);
 	};
 }
