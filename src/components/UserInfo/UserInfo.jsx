105e3c3d45c7598a86ac1acaa11b41e0b0efa92e
Falenos <georgis.kakavas@gmail.com>
2019-01-07 01:35:10 +0200

    Added moment and fixed date format.

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import moment from 'moment';
 
 const UserInfo = ({userData}) => {
 	const {name, login, created_at, bio, public_gists, public_repos, followers} = userData;
+	const createdAt = moment(new Date(created_at)).format('LL');
 	return (
 		<div>
 			<hr/>
 			<h2>About this User</h2>
-			<div>{`${name} is a GitHub community member since ${created_at}`}</div>
+			<div>{`${name} is a GitHub community member since ${createdAt}`}</div>
 			{bio && <div>{bio}</div>}
 			<div>At his <a target='_blank' href={`https://github.com/${login}`}>profile</a> page you can explore {public_repos} public Repositories and {public_gists} Gists</div>
 			<hr/>
